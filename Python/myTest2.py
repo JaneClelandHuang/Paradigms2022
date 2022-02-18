f9288969040065be0e4a642a1ebf09ef4ac1850b
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2022-02-17 19:46:26 -0500

    Create myTest2.py

---
+++ Python/myTest2.py
@@ -0,0 +1,11 @@
+import abc
+
+class State(metaclass=abc.ABCMeta):
+    def __init__(self):
+        print ("Hello")
+
+class Myclass(State):
+    def __init__(self):
+        print("My class")
+
+mc = Myclass()
