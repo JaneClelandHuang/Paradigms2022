9e6f697a9e464024e07e5156ffaec41cc75e8e91
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2022-03-29 21:36:31 -0400

    Create HelloWorldJUnit.java

---
+++ Java/tests/HelloWorldJUnit.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+public class HelloWorldJUnit {
+    @Test
+    public void test() {
+        assertEquals("Hello world","hello world");  
+    }
+}
