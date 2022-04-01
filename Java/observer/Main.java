bbb71949edce76ecefa4803ba8dc34511aa697b4
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2022-04-01 08:11:07 -0400

    Create Main.java

---
+++ Java/observer/Main.java
@@ -0,0 +1,10 @@
+public class Main
+{
+    public static void main(String[] args) {
+        Mouse mouse = new Mouse();
+        Cat cat1 = new Cat();
+        Cat cat2 = new Cat();
+        mouse.registerObserver(cat1);
+        mouse.registerObserver(cat2);
+    }
+}
