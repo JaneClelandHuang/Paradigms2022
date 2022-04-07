12f97e5c610d01f6ebed6b83d534402399d2e32e
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2022-04-07 19:28:33 -0400

    Create transientexample.clj

---
+++ Clojure/src/transientexample.clj
@@ -0,0 +1,21 @@
+(ns transientexample
+   (:gen-class))
+
+(defn vrange [n]
+   (loop [i 0 v []]
+		(if (< i n)
+			(recur (inc i) (conj v i))
+	v)))
+	
+(defn vrange2 [n]
+   (loop [i 0 (transient [])]
+		(if (< i n)
+			(recur (inc i) (conj! v i))
+	(persistent! v))))
+	   
+(defn run [opts]
+   (println "Non transient")
+   (time (vrange 100000))
+   (println "Transient")
+   (time (vrange2 100000)))
+   
