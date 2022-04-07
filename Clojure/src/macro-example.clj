81ffc956513b97e5568c8093167b4057eafa9766
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2022-04-07 19:50:46 -0400

    Create macro-example.clj

---
+++ Clojure/src/macro-example.clj
@@ -0,0 +1,14 @@
+(ns macro-example
+   (:gen-class))
+
+(defmacro eval-infix [form]
+	(list (second form) (first form) (second (rest form))))
+
+(defn run [opts]
+   (use 'clojure.test)
+   (eval-infix (20 * 7 ))
+   (* 20 7)
+   (is (= (eval-infix (20 * 7 )) 140))
+   (is (= (macroexpand '(eval-infix (20 * 7 ))) '(* 20 7)))
+)
+   
