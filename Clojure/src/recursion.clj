ae987237ee73e366d8a6576e1205fc5773153e80
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2022-04-07 19:13:33 -0400

    Create recursion.clj

---
+++ Clojure/src/recursion.clj
@@ -0,0 +1,12 @@
+(ns recursion
+   (:gen-class))
+
+(defn factorial [n]
+   (loop [cnt n acc 1]
+		(if (zero? cnt)
+			(printf "The factorial of %d is %d" n acc)
+			(recur (dec cnt) (* acc cnt))
+)))
+	   
+(defn run [opts]
+   (factorial n))
