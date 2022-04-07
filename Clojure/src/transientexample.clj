63134581bd0635c381865d002d8d1c9142da5694
Jane Cleland Huang <JaneClelandHuang@nd.edu>
2022-04-07 19:31:30 -0400

    Update transientexample.clj

--- Clojure/src/transientexample.clj
+++ Clojure/src/transientexample.clj
@@ -7,15 +7,9 @@
 			(recur (inc i) (conj v i))
 	v)))
 	
-(defn vrange2 [n]
-   (loop [i 0 (transient [])]
-		(if (< i n)
-			(recur (inc i) (conj! v i))
-	(persistent! v))))
-	   
+  
 (defn run [opts]
    (println "Non transient")
    (time (vrange 100000))
-   (println "Transient")
-   (time (vrange2 100000)))
+)
    
